e0f351d3befbfc299ee6a31ebbb8e66b89af9a10
kettle11 <ian.kettlewell@gmail.com>
2015-02-01 01:41:11 -0500

    player / painting code and project files

---
+++ Paint Globe/Assets/PlanetScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = 
+		new Texture2D(400,
+		              400,
+		              TextureFormat.RGB24, false);
+	}
+
+	void PaintAtPoint(int x, int y, Color color)
+	{
+		Texture2D tex = ((Texture2D)renderer.material.mainTexture);
+		tex.SetPixel(x, y, color);
+		tex.Apply();
+
+	}
+
+	int radius = 5;
+
+	public void PaintAtUV(Vector2 uv , Color color)
+	{
+
+		Texture2D tex = ((Texture2D)renderer.material.mainTexture);
+
+		int xCenter = (int)(uv.x * tex.width);
+		int yCenter = (int)(uv.y * tex.height);
+
+		for(int i = -radius; i < radius; i++)
+		{
+			for(int j = -radius; j < radius; j++)
+			{
+				if(Mathf.Sqrt(i*i+j*j) < radius)
+				{
+					tex.SetPixel((int)(uv.x * tex.width) + i, (int)(uv.y * tex.height) + j, color);
+				}
+			}
+		}
+
+		//tex.SetPixel((int)(uv.x * tex.width), (int)(uv.y * tex.height), Color.red);
+		tex.Apply();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Paint Globe/Assets/Player.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float planetRadius = 10f;
+	public Vector3 planetCenter;
+	public PlanetScript planet;
+
+	public float speed = .5f;
+
+	public Color color = Color.blue;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		RaycastHit hit;
+		Vector3 gravityDir = (planetCenter - this.transform.position).normalized;
+
+		transform.up = -gravityDir;
+
+
+		if(Input.GetKey(KeyCode.D))
+		{
+			transform.position += Camera.main.transform.right * speed;
+		}
+
+		if(Input.GetKey(KeyCode.A))
+		{
+			transform.position -= Camera.main.transform.right * speed;
+		}
+
+		if(Input.GetKey(KeyCode.W))
+		{
+			transform.position += Camera.main.transform.up * speed;
+		}
+
+		if(Input.GetKey(KeyCode.S))
+		{
+			transform.position -= Camera.main.transform.up * speed;
+		}
+
+		//Debug.DrawLine(transform.position, transform.position + (planetCenter - this.transform.position), Color.red);
+
+		if(Physics.Raycast(new Ray(this.transform.position - gravityDir * 1f, planetCenter - this.transform.position), out hit))
+		{
+			transform.position = hit.point + transform.up * .5f * renderer.bounds.size.y;
+			planet.PaintAtUV(hit.textureCoord, color);
+		}
+	}
+}
